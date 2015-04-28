0e2e9adc5beb6dd44bdb6f244e3ed8cfcd510a52
raluka <badoi.raluca@gmail.com>
2015-04-28 18:41:03 +0200

    Exercise 7: More Printing

---
+++ ex7.rb
@@ -0,0 +1,29 @@
+=begin
+
+Exercise 7: More Printing
+
+Now we are going to do a bunch of exercises where you just type code in and make it run. I won't be explaining this
+exercise because it is more of the same. The purpose is to build up your chops.
+
+=end
+
+puts "Mary has a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "." * 10 # prints 10 dots
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# puts add a new line after executing
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
